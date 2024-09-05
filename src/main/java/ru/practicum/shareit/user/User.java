3b36bfdd2fd07f83300d55624e826e0c75947c06
Maxim Myasnenko <MaxChip322@yandex.ru>
2024-09-06 00:45:40 +0700

    Add validation for user model

--- src/main/java/ru/practicum/shareit/user/User.java
+++ src/main/java/ru/practicum/shareit/user/User.java
@@ -1,6 +1,10 @@
 package ru.practicum.shareit.user;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
 import lombok.Data;
+import ru.practicum.shareit.user.markers.onUserCreate;
+import ru.practicum.shareit.user.markers.onUserUpdate;
 
 /**
  * TODO Sprint add-controllers.
@@ -8,6 +12,9 @@ import lombok.Data;
 @Data
 public class User {
     private Long id;
+    @NotEmpty(groups = onUserCreate.class, message = "Name must be present")
     private String name;
+    @NotEmpty(groups = onUserCreate.class, message = "Email must be present")
+    @Email(groups = {onUserCreate.class, onUserUpdate.class}, message = "Email should be valid")
     private String email;
 }
