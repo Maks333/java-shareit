21271cebed50cf80cd24b1190355c8da3d5a38fe
Maxim Myasnenko <MaxChip322@yandex.ru>
2024-09-05 23:26:32 +0700

    Implement UserController interface

--- src/main/java/ru/practicum/shareit/user/UserController.java
+++ src/main/java/ru/practicum/shareit/user/UserController.java
@@ -1,12 +1,34 @@
 package ru.practicum.shareit.user;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * TODO Sprint add-controllers.
  */
 @RestController
+@RequiredArgsConstructor
 @RequestMapping(path = "/users")
 public class UserController {
+    private final UserService service;
+
+    @GetMapping("/{userId}")
+    public User get(@PathVariable long userId) {
+        return service.findById(userId);
+    }
+
+    @PostMapping
+    public User create(@RequestBody User user) {
+        return service.create(user);
+    }
+
+    @PatchMapping("/{userId}")
+    public User update(@PathVariable long userId, @RequestBody User user) {
+        return service.update(userId, user);
+    }
+
+    @DeleteMapping("/{userId}")
+    public void delete(@PathVariable long userId) {
+        service.delete(userId);
+    }
 }
