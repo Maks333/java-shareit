c6b795c5200f2006e6ac211636a3e4ab879b0298
Maxim Myasnenko <MaxChip322@yandex.ru>
2024-09-06 04:29:09 +0700

    Create Item controller

--- src/main/java/ru/practicum/shareit/item/ItemController.java
+++ src/main/java/ru/practicum/shareit/item/ItemController.java
@@ -1,12 +1,45 @@
 package ru.practicum.shareit.item;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+import ru.practicum.shareit.item.dto.ItemDto;
+import ru.practicum.shareit.item.model.Item;
+
+import java.util.List;
 
 /**
  * TODO Sprint add-controllers.
  */
+@RequiredArgsConstructor
 @RestController
 @RequestMapping("/items")
 public class ItemController {
+    private final ItemService service;
+    @PostMapping
+    public Item create(@RequestHeader("X-Sharer-User-Id") long userId,
+                       @RequestBody ItemDto itemDto) {
+        return service.create(userId, itemDto);
+    }
+
+    @PatchMapping("/{itemId}")
+    public Item update(@RequestHeader("X-Sharer-User-Id") long userId,
+                       @PathVariable long itemId, ItemDto itemDto) {
+        return service.update(userId, itemId, itemDto);
+    }
+
+    @GetMapping("/{itemId}")
+    public Item get(@RequestHeader("X-Sharer-User-Id") long userId,
+                    @PathVariable long itemId) {
+        return service.findById(userId, itemId);
+    }
+
+    @GetMapping
+    public List<Item> getAll(@RequestHeader("X-Sharer-User-Id") long userId) {
+        return service.findAll(userId);
+    }
+
+    @GetMapping("/search")
+    public List<Item> searchAll(@RequestParam String text) {
+        return service.searchAll(text);
+    }
 }
