78dc834c48d586d2b9739bb0991fd7fafa3bcc69
Maxim Myasnenko <MaxChip322@yandex.ru>
2024-09-19 08:35:58 +0700

    Create BookingController

--- src/main/java/ru/practicum/shareit/booking/BookingController.java
+++ src/main/java/ru/practicum/shareit/booking/BookingController.java
@@ -1,12 +1,49 @@
 package ru.practicum.shareit.booking;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 /**
  * TODO Sprint add-bookings.
  */
 @RestController
+@RequiredArgsConstructor
 @RequestMapping(path = "/bookings")
 public class BookingController {
+    private final BookingService service;
+
+    @PostMapping
+    public Booking createBooking(@RequestHeader("X-Sharer-User-Id") long userId,
+                                 Booking booking) {
+        return service.createBooking(userId, booking);
+    }
+
+    @PatchMapping("/{bookingId}")
+    public Booking changeBookingStatus(@RequestHeader("X-Sharer-User-Id") long userId,
+                                       @PathVariable long bookingId,
+                                       @RequestParam("approved") boolean approved) {
+        return service.changeBookingStatus(approved);
+    }
+
+    @GetMapping("/{bookingId}")
+    public Booking getBooking(@RequestHeader("X-Sharer-User-Id") long userId,
+                              @PathVariable long bookingId) {
+        return service.getBooking(bookingId);
+    }
+
+    @GetMapping
+    public List<Booking> getAllBookingsMadeByUser
+            (@RequestHeader("X-Sharer-User-Id") long userId,
+             @RequestParam(name = "state", required = false, defaultValue = "ALL") BookingState state) {
+        return service.getAllBookingsMadeByUser(userId, state);
+    }
+
+    @GetMapping("/owner")
+    public List<Booking> getAllBookingsForAllItemsOfUser
+            (@RequestHeader("X-Sharer-User-Id") long userId,
+             @RequestParam(name = "state", required = false, defaultValue = "ALL") BookingState state) {
+        return service.getAllBookingsForAllItemsOfUser(userId, state);
+    }
 }
