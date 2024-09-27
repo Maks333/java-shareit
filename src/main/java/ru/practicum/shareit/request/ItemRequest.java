534308ef89d8a5c9f1f3e6207e0214acf52bdb4c
Maxim Myasnenko <MaxChip322@yandex.ru>
2024-09-27 12:44:46 +0700

    Create ItemRequest model

--- src/main/java/ru/practicum/shareit/request/ItemRequest.java
+++ src/main/java/ru/practicum/shareit/request/ItemRequest.java
@@ -1,7 +1,20 @@
 package ru.practicum.shareit.request;
 
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.time.Instant;
+
 /**
  * TODO Sprint add-item-requests.
  */
+@Data
+@Entity
+@Table(name = "requests")
 public class ItemRequest {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String description;
+    private Instant created = Instant.now();
 }
