b2ad85982b3949cb53f229169b6628b322107977
Maxim Myasnenko <MaxChip322@yandex.ru>
2024-09-29 16:55:20 +0700

    Add ItemRequest Tests

--- server/src/main/java/ru/practicum/shareit/request/dto/ItemRequestDto.java
+++ server/src/main/java/ru/practicum/shareit/request/dto/ItemRequestDto.java
@@ -1,6 +1,8 @@
 package ru.practicum.shareit.request.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -9,6 +11,8 @@ import java.util.List;
  * TODO Sprint add-item-requests.
  */
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class ItemRequestDto {
     private Long id;
     private String description;
